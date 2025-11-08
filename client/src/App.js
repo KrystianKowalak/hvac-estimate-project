1ee2707f3f5187984b30790c09d01538dd4b214c
Cossack <kkowalak9@gmail.com>
2025-11-07 19:41:08 -0800

    Created a fresh client side with a simple title and icon.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>HVAC Estimate Form</h1>
+      <p>Welcome! This will soon be your technician interface.</p>
+    </div>
+  );
+}
+
+export default App;
